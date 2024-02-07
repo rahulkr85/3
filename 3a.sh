3b0006a9cc4b06d9d24f44bec107155fa7ca81e5
Rahul kumar <152591373+rahulkr85@users.noreply.github.com>
2024-02-08 00:17:15 +0530

    Update 3a.sh

--- 3a.sh
+++ 3a.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-for i in $*
+  GNU nano 7.2                      3a.sh                               # !/bin/bash
+# !/bin/bash
+for i in $* 
 do
 if [ -d $i ]
 then
-cmd=$(ls -Rl $1 | grep "^-" | tr -s ' ' | cut –d ' ' -f 5 | sort –n | t>echo “The largest file size is $cmd”
+cmd=$(ls -Rl $1 | grep "^-" | tr -s '  ' | cut –d '  ' -f 5 | sort –n | tail -1)
+echo “The largest file size is $cmd”
 else
 echo "not directory"
 fi
 done
+
+
